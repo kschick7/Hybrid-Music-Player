6cb15c2ef527c877d88c4c8555f7d1873a46c383
Kyle Schick <kschick@umich.edu>
2016-04-16 03:55:54 -0400

    Implemented Youtube searching api

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from flask import Flask, render_template, session
 # Initialize Flask app with the template folder address
 app = Flask(__name__)
 
+API_KEY = open('api_key').readline()
+
 @app.route('/')
 def index_route():
-	return render_template('index.html')
+	return render_template('index.html', api_key=API_KEY)
 
 # Register the controllers
 # app.register_blueprint(index.index)
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,35 @@
+var YOUTUBE_BASE_URL = 'http://www.youtube.com/embed/<id>?autoplay=0';
+// var API_KEY = document.getElementById('api_key').innerHTML;
+var API_KEY = "";
+var SEARCH_API_BASE_URL = 
+	"https://www.googleapis.com/youtube/v3/search?part=snippet&q=<q>&key=<key>";
+
+function setApiKey(key) {
+	API_KEY = key;
+}
 
 function search() {
 	var query = document.getElementById('search_query').value;
-	alert(query);
+	var url = SEARCH_API_BASE_URL.replace('<q>', query);
+	url = url.replace('<key>', API_KEY);
+	var req = new XMLHttpRequest();
+	req.open("GET", url, false);
+	req.setRequestHeader(
+		"Content-type",
+		"application/json"
+	);
+	req.send(null);
+	var items = JSON.parse(req.responseText)['items'];
+
+	loadVideo(items[0].id.videoId);
+}
+
+function getURL(id) {
+	return YOUTUBE_BASE_URL.replace('<id>', id);
+}
+
+function loadVideo(id) {
+	var url = getURL(id);
+	document.getElementById('video_player').src = url;
+	document.getElementById('video_player').style.display = "";
 }
