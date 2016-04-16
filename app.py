cd6289151f8b9fd434255e5b258d4ef77c114e80
Kyle Schick <kschick@umich.edu>
2016-04-16 02:48:52 -0400

    Initial file structure

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, session
+# import index
+# import controllers
+# import api
+
+# Initialize Flask app with the template folder address
+app = Flask(__name__)
+
+@app.route('/')
+def index_route():
+	return render_template('index.html')
+
+# Register the controllers
+# app.register_blueprint(index.index)
+# app.register_blueprint(api.userapi)
+
+# Listen on external IPs
+# For us, listen to port 3000 so you can just run 'python app.py' to start the server
+if __name__ == '__main__':
+    # listen on external IPs
+    app.run(host='0.0.0.0', port=4000, debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,5 @@
+
+function search() {
+	var query = document.getElementById('search_query').value;
+	alert(query);
+}
