1382c58cff6d0cefc502d8eb9553ddb2c882de41
Kyle Schick <kschick@umich.edu>
2016-04-16 04:52:50 -0400

    Cleaned up code

--- static/js/index.js
+++ static/js/index.js
@@ -1,44 +1,34 @@
 var YOUTUBE_BASE_URL = 'http://www.youtube.com/embed/<id>?autoplay=1&enablejsapi=1';
-// var API_KEY = document.getElementById('api_key').innerHTML;
 var API_KEY = "";
 var SEARCH_API_BASE_URL = 
 	"https://www.googleapis.com/youtube/v3/search?part=snippet&q=<q>&key=<key>";
 var player;
 var playing = true;
 
+
+// Sets the Youtube list api key, necessary to send search requests
 function setApiKey(key) {
 	API_KEY = key;
 }
 
-function createPlayer() {
+// Loads Youtube's Iframe Api
+function loadYoutubeIFrame() {
 	var tag = document.createElement('script');
 	tag.src = "https://www.youtube.com/iframe_api";
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
+// Called when Youtube iframe api is finished loading, creates player
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height: '390',
 		width: '640',
 		videoId: 'M7lc1UVf-VE',
-		// events: {
-		// 	'onReady': onPlayerReady,
-		// 	'onStateChange': onPlayerStateChange
-		// }
 	});
 }
 
-function onPlayerReady(event) {
-	event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(stopVideo, 6000);
-		done = true;
-	}
-}
+// Toggles player between playing and paused
 function changeState() {
 	button = document.getElementById('playerControl');
 	if (playing) {
@@ -51,6 +41,7 @@ function changeState() {
 	playing = !playing;
 }
 
+// Sends search query over Youtube's API. Load's first result
 function search() {
 	var query = document.getElementById('search_query').value;
 	var url = SEARCH_API_BASE_URL.replace('<q>', query);
@@ -63,28 +54,11 @@ function search() {
 	);
 	req.send(null);
 	var items = JSON.parse(req.responseText)['items'];
-
 	loadVideo(items[0].id.videoId);
 }
 
-function getURL(id) {
-	return YOUTUBE_BASE_URL.replace('<id>', id);
-}
-
+// Load's the specified video (by id)
 function loadVideo(id) {
-	var url = getURL(id);
+	var url = YOUTUBE_BASE_URL.replace('<id>', id);
 	player.loadVideoById(id);
 }
-
-// function pauseVideo() {
-// 	alert(document.getElementById('player_iframe').contentWindow);
-// }
-
-// function toggleVideo(state) {
-//	 // if state == 'hide', hide. Else: show video
-//	 var div = document.getElementById("video_player");
-//	 var iframe = document.getElementById("player_iframe").contentWindow;
-//	 div.style.display = state == 'hide' ? 'none' : '';
-//	 func = state == 'hide' ? 'pauseVideo' : 'playVideo';
-//	 iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
-// }
